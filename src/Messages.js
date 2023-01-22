1d858d14855ea125ed4832cfd1a6f424d611bf1a
Tate <tatebraeckel@gmail.com>
2023-01-22 15:05:36 -0700

    commit 1

---
+++ src/Messages.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+function Messages() {
+    return (
+        <div>
+            <ul>
+                <li> Message</li>
+                <li> Message</li>
+                <li> Message</li>
+                <li> Message</li>
+                <li> Message</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Messages;
---
+++ src/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function SignUp() {
+    return (
+        <>
+            <button type="submit" value="Submit">Sign Up</button>
+        </>
+    )
+}
+
+export default SignUp;
